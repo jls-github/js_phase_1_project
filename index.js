cc1017fce15909a01b918071923120487a1c54ca
Eric Nganga <ericmainaedits@gmail.com>
2021-04-22 17:46:45 -0500

    create quote div

--- index.js
+++ index.js
@@ -6,3 +6,23 @@ const getQuote = () => {
     .then(res => res.json())
 }
     getQuote().then(console.log)
+
+    //create components
+
+    const createQuoteDiv = (quote) => {
+        const quoteContainer = document.createElement('div')
+        const sentence = document.createElement('p')
+        const character = document.createElement('h3')
+
+        quote.innerText = sentence
+        character.innerText = character['name']
+
+        quoteContainer.append(sentence, character)
+
+        return quoteContainer 
+    }
+     
+    getQuote().then((quote) => {
+        const quoteDiv = createQuoteDiv(quote)
+        console.log(quoteDiv)
+    })
