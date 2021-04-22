9f844ae03a6e887d15945d836f9f362cd9681748
Eric Nganga <ericmainaedits@gmail.com>
2021-04-22 15:23:07 -0500

    create file structure

--- index.js
+++ index.js
@@ -1,7 +1,1 @@
-const getQuote = () => {
-    fetch(`https://game-of-thrones-quotes.herokuapp.com/v1/random`)
-    .then(res => res.json())
-
-}
-
-getQuote().then(console.log)
+console.log('hello')
