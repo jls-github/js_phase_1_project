73cd87d34ffc53a5e9b365ce66369819ce589300
Eric Nganga <ericmainaedits@gmail.com>
2021-04-22 16:16:09 -0500

    fetch data

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log('hello')
+const baseUrl = `https://game-of-thrones-quotes.herokuapp.com/` 
+const url = `${baseUrl}v1/random`
+
+const getQuote = () => {
+    return fetch(url)
+    .then(res => res.json())
+}
+    getQuote().then(console.log)
