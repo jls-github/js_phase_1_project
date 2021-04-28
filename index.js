a69b51de8fba0a975705dd9dc43b405c0593bb99
John Souza <souzajohnlee@gmail.com>
2021-04-28 11:37:01 -0700

    Use fallback Api

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const baseUrl = `https://game-of-thrones-quotes.herokuapp.com/` 
+const newUrl = 'https://got-quotes.herokuapp.com/quotes'
 const url = `${baseUrl}v1/random`
 
 const getQuote = () => {
-    return fetch(url)
+    return fetch(newUrl)
     .then(res => res.json())
 }
     getQuote().then(console.log)
@@ -21,21 +22,31 @@ const createQuoteDiv = (quote) => {
 
     return quoteContainer
 }
+
+const createQuoteDivFromFallBackApi = quote => {
+    const quoteContainer = document.createElement('div')
+    const quote = document.createElement('p')
+    const character = document.createElement('footer')
+
+    quote.innerText = quote.quote
+    character.innerText = quote.character
+
+    quoteContainer.append(quote,character)
+
+    return quoteContainer
+}
+
  const appendQuote= (quoteDiv) => {
     const quoteContainer = document.getElementById('quote-container')
     quoteContainer.innerHTML = ""
     quoteContainer.append(quoteDiv)
-
-
  }
 
 const generateQuote = () => {
     getQuote().then((quote) => {
-        const quoteDiv = createQuoteDiv(quote)
+        const quoteDiv = createQuoteDivFromFallBackApi(quote)
         appendQuote(quoteDiv)
-    
     })
-    
 }
 
 const newQuoteBtn = document.getElementById('new-quote-btn')
